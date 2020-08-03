68a5aa717b3b4760a04edd20260b33dce9f22d29
NidoKota <36328961+NidoKota@users.noreply.github.com>
2020-08-03 12:46:42 +0900

    細かいミス

--- Assets/Scripts/ScenarioController/Editor/ScenarioInputEditor.cs
+++ Assets/Scripts/ScenarioController/Editor/ScenarioInputEditor.cs
@@ -211,7 +211,7 @@ namespace ScenarioController.Editor
                             yMin = propertyHight,
                             height = EditorGUIUtility.singleLineHeight
                         };
-                        EditorGUI.HelpBox(helpBoxRect, "Deletes or copies the specified area.", MessageType.None);
+                        EditorGUI.HelpBox(helpBoxRect, "Deletes or Copies the specified area.", MessageType.None);
                         propertyHight = helpBoxRect.yMax + 2;
 
                         Rect minValueRect = new Rect()
